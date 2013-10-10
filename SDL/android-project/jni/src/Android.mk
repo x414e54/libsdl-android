acdfe1bb75bb2cfcf5273f32abc538167fe7a7e5
Denis Bernard <denis@wildservices.net>
2013-10-11 00:22:07 +0200

    Made the static build easier to use

--- SDL/android-project/jni/src/Android.mk
+++ SDL/android-project/jni/src/Android.mk
@@ -4,11 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-LOCAL_SRC_FILES := SDL_android_main.c main.c
+LOCAL_SRC_FILES := main.c
 
 LOCAL_STATIC_LIBRARIES := SDL2_static
-
-LOCAL_LDLIBS := -lGLESv1_CM -llog
 
 include $(BUILD_SHARED_LIBRARY)
 $(call import-module,SDL)LOCAL_PATH := $(call my-dir)
